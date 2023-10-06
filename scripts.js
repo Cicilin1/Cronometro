f7b9f33ef3d94af20043b87891b3e81a8e0a0e29
Cicilini <guilherme.cicilini@hotmail.com>
2023-10-05 21:13:12 -0300

    design-responsividade

--- scripts.js
+++ scripts.js
@@ -1,83 +1,107 @@
 const secunds = document.querySelector('.secunds');
 const minutes = document.querySelector('.minutes');
+const miliseconds = document.querySelector('.miliseconds');
 const hours = document.querySelector('.hours');
+
 const btn1 = document.querySelector('.play');
 const btn2 = document.querySelector('.pause');
-const btn3 = document.querySelector('.reset')
+const btn3 = document.querySelector('.reset');
+const btn4 = document.querySelector('.return')
 
 
-let run = false;
+
+let ispaused = false;
 let interval;
+let ml = 0;
 let s = 0;
 let m = 0;
 let h = 0;
 
-function setButtonStyle(activeButton) {
-    btn1.style.backgroundColor = 'rgba(60, 60, 60, 1)';
-    btn2.style.backgroundColor = 'rgba(60, 60, 60, 1)';
-    btn3.style.backgroundColor = 'rgba(60, 60, 60, 1)';
-    activeButton.style.backgroundColor = '#1a1818';
-}
-
 function play() {
-    setButtonStyle(btn1);
+    interval = setInterval(() => {
+        if (!ispaused) {
+            ml += 10;
 
-    if (!run) {
-        interval = setInterval(() => {
-            s++;
-            if (s <= 9) {
-                secunds.innerHTML = `0${s}`;
-            } else if (s >= 10 && s < 60) {
-                secunds.innerHTML = s;
-            } else if (s === 60) {
-                s = 0;
-                m++;
-                secunds.innerHTML = `00`
-                if (m <= 9) {
-                    minutes.innerHTML = `0${m}`;
-                } else if (m >= 10 && m < 60) {
-                    minutes.innerHTML = m;
-                } else if (m == 60) {
-                    m = 0;
-                    h++;
-                    if (h <= 9) {
-                        hours.innerHTML = `0${h}`;
-                    }
-                    else if (h <= 9) {
-                        hours.innerHTML = h;
-                    }
-                }
+            if (ml === 1000) {
+                s++;
+                ml = 0;
             }
 
-        }, 1000)
-        run = true;
+            if (s === 60) {
+                m++;
+                s = 0;
+            }
+
+            if (h === 60) {
+                h++;
+                m = 0;
+            }
+
+            miliseconds.textContent = formatml(ml);
+            secunds.textContent = format(s);
+            minutes.textContent = format(m);
+            hours.textContent = format(h);
+        }
+    }, 10);
+
+    btn1.style.display = "none"
+    btn2.style.display = "inline-block"
+}
+
+function format(time) {
+    return time < 10 ? `0${time}` : time;
+}
+
+function formatml(mls) {
+    if (mls < 10) {
+        return `00${mls}`;
+    } else if (mls < 100) {
+        return `0${mls}`;
+    } else {
+        return mls;
     }
 }
 
 function pause() {
-    setButtonStyle(btn2);
+    clearInterval(interval);
+    ispaused = true;
 
-    clearInterval(interval)
-    run = false;
+    btn2.style.display = "none"
+    btn4.style.display = "inline-block"
 }
 
+function returnInvterval(){
+    ispaused = false;
+    play()
+
+    btn2.style.display = "inline-block"
+    btn4.style.display = "none"
+}
+
+
 function reset() {
-    setButtonStyle(btn3);
-
-    clearInterval(interval)
-
+    clearInterval(interval);
+    ml = 0;
     s = 0;
     m = 0;
     h = 0;
 
-    secunds.textContent = "00";
-    minutes.textContent = "00";
-    hours.textContent = "00";
+    miliseconds.textContent = formatml(ml);
+    secunds.textContent = format(s);
+    minutes.textContent = format(m);
+    hours.textContent = format(h);
 
-    run = false;
+    ispaused = false;
+
+    btn1.style.display = "inline-block"
+    btn4.style.display = "none"
+    btn2.style.display = "none"
+
 }
 
 
 
-
-
+btn1.addEventListener('click', play);
+btn2.addEventListener('click', pause);
+btn3.addEventListener('click', reset);
+btn4.addEventListener('click', returnInvterval)
